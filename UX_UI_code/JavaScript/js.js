7bd0416cc2495f09ccd2955b6cdcbc0bb6e20575
AmandaJakse <73820132+amandajakse@users.noreply.github.com>
2021-04-19 13:12:51 +0200

    Fixed problem with map on home page

--- UX_UI_code/JavaScript/js.js
+++ UX_UI_code/JavaScript/js.js
@@ -1,10 +1,10 @@
 //Here we add JavaScript
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 
@@ -14,3 +14,10 @@ document.getElementById("current_date").innerHTML = "Today is " + d.toDateString
 
 
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW15LWtyaSIsImEiOiJja201eXRiMWswajBnMnhrbjF0NjJrMjg5In0.gzeG_pFW93uuh_DvYJ2lIQ';
+var map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/amy-kri/cknn8knez0jwz17kutezjyxwl', // style URL
+    center: [10.116, 56.170], // starting position [lng, lat]
+    zoom: 15 // starting zoom
+});
